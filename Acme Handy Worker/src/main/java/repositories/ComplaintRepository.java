50832ed6ad0d00440b6d5669992b98abd423d4f2
Urabity <juaparser@gmail.com>
2018-11-23 20:52:10 +0100

    Actualización repositorio, servicio y test con nuevas queries

--- Acme Handy Worker/src/main/java/repositories/ComplaintRepository.java
+++ Acme Handy Worker/src/main/java/repositories/ComplaintRepository.java
@@ -1,5 +1,7 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,5 +15,16 @@ public interface ComplaintRepository extends JpaRepository<Complaint, Integer>{
 	@Query("select a from Actor a where a.id = '?1'") 
 	Complaint findOne(Integer Id);
 	
+	// B-RF 36.1
+	@Query("select c from Complaint c where c not in (select distinct r.complaint from Report r)")
+	Collection<Complaint> getComplaintsWithNoReports();
+	
+	// B-RF 36.2
+	@Query("select r.complaint from Report r where r.referee.id = ?1")
+	Collection<Complaint> getComplaintsReferee(int refereeId);
+	
+	// B-RF 37.3
+	@Query("select distinct fx.complaints from Application a join a.fixUpTask fx where a.handyWorker.id = ?1")
+	Collection<Complaint> getComplaintsHandyWorker(int handyWorkerId);
 }
 
