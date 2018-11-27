c5363a13368d2327e66d8a1a0620485a10de0001
gonzaloguiher <zalo.agui1@gmail.com>
2018-11-27 23:21:46 +0100

    Actualizacion y correccion servicio y test de mensaje

--- Acme Handy Worker/src/test/java/services/MessageServiceTest.java
+++ Acme Handy Worker/src/test/java/services/MessageServiceTest.java
@@ -1,5 +1,7 @@
 package services;
 
+import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import domain.Actor;
+import domain.Application;
 import domain.Box;
+import domain.Customer;
+import domain.HandyWorker;
 import domain.Message;
 
 import utilities.AbstractTest;
@@ -19,6 +24,9 @@ import utilities.AbstractTest;
 		"classpath:spring/config/packages.xml" })
 @Transactional
 public class MessageServiceTest extends AbstractTest {
+
+	@Autowired
+	private ApplicationService applicationService;
 
 	@Autowired
 	private MessageService messageService;
@@ -32,10 +40,13 @@ public class MessageServiceTest extends AbstractTest {
 	@Test
 	public void testCreate() {
 
-		Actor sender = actorService.findOne(14580); // HandyWorker5
-		Actor recipient = actorService.findOne(14570); // Admin1
+		Actor sender = actorService.findOne(15730); // HandyWorker5
+		Actor recipient = actorService.findOne(15720); // Admin1
 
 		Message message = messageService.create(sender, recipient);
+
+		// System.out.println("Sender" + message.getSender());
+		// System.out.println("Recipient" + message.getRecipient());
 
 		Assert.isTrue(message.getPriority() != "");
 		Assert.isTrue(message.getTags().isEmpty());
@@ -45,10 +56,14 @@ public class MessageServiceTest extends AbstractTest {
 
 	@Test
 	public void testSave() {
-		Actor sender = actorService.findOne(14580); // HandyWorker5
-		Actor recipient = actorService.findOne(14570); // Admin1
+		Actor sender = actorService.findOne(15730); // HandyWorker5
+		Actor recipient = actorService.findOne(15720); // Admin1
 
 		Message message = messageService.create(sender, recipient);
+
+		// System.out.println("Sender" + message.getSender());
+		// System.out.println("Recipient" + message.getRecipient());
+
 		Message result;
 
 		message.setBody("Cuerpo del mensaje");
@@ -59,49 +74,107 @@ public class MessageServiceTest extends AbstractTest {
 		Assert.isTrue(messageService.findAll().contains(result));
 	}
 
+	// Box 7
+	// Box 11
+
 	@Test
 	public void testDelete() {
 
-		authenticate("customer3");
+		authenticate("handyWorker1");
 
-		// Message 1 -> 14793; Sender -> 14571 Customer1; Recipient -> 14572
-		// Customer2
-		// Message 2 -> 14794; Sender -> 14572 Customer2; Recipient -> 14571
-		// Customer1
-		// Message 3 -> 14795; Sender -> 14573 Customer3; Recipient -> 14576
-		// HandyWorker1
+		// Message 1 -> 15943; Sender -> 15721 Customer 1; Recipient -> 15722
+		// Customer 2
+		// Message 2 -> 15944; Sender -> 15722 Customer 2; Recipient -> 15721
+		// Customer 1
+		// Message 3 -> 15945; Sender -> 15723 Customer 3; Recipient -> 15726
+		// HandyWorker 1
 
-		Message message = messageService.findOne(14795);
+		// HandyWorker 1; Box 25 (Spam Box) -> 15907
+		// HandyWorker 1; Box 26 (In Box) -> 15908
+		// HandyWorker 1; Box 27 (Out Box) -> 15909
+		// HandyWorker 1; Box 28 (Trash Box) -> 15910
+
+		// Actor actor = actorService.findOne(15726);
+
+		Box box = boxService.findOne(15907);
+		Box box2 = boxService.findOne(15908);
+		Box box3 = boxService.findOne(15909);
+		Box box4 = boxService.findOne(15910);
+
+		Collection<Box> boxes = boxService.findAll();
+
+		Message message = messageService.findOne(15945);
 		messageService.delete(message);
-		// Assert.isTrue(!messageService.findAll().contains(message) ||
-		// boxService.);
+
+		Assert.isTrue(!boxes.contains(message));
+		Assert.isTrue((!box.getMessages().contains(message)
+				&& !box2.getMessages().contains(message)
+				&& !box3.getMessages().contains(message) && box4.getMessages()
+				.contains(message))
+				|| (!box.getMessages().contains(message)
+						&& !box2.getMessages().contains(message)
+						&& !box3.getMessages().contains(message) && !box4
+						.getMessages().contains(message)));
 
 		unauthenticate();
 	}
 
+	// Application 5 -> 15975; HandyWorker 2 -> 15727;
+	// FixUpTask 4 -> 15949; Customer 4 -> 15724
+
+	// HandyWorker 2; Box 29 (Spam Box) -> 15911
+	// HandyWorker 2; Box 30 (In Box) -> 15912
+	// HandyWorker 2; Box 31 (Out Box) -> 15913
+	// HandyWorker 2; Box 32 (Trash Box) -> 15914
+
+	// Customer 4; Box 13 (Spam Box) -> 15895
+	// Customer 4; Box 14 (In Box) -> 15896
+	// Customer 4; Box 15 (Out Box) -> 15897
+	// Customer 4; Box 16 (Trash Box) -> 15898
+
 	@Test
 	public void testSystemMessage() {
 
+		Application application = applicationService.findOne(15975);
+
+		Customer customer = application.getFixUpTask().getCustomer();
+		HandyWorker handyWorker = application.getHandyWorker();
+
+		Assert.isTrue(customer.getId() == 15724);
+		Assert.isTrue(handyWorker.getId() == 15727);
+
+		messageService.sendSystemMessages(application);
+
+		Box customerBox = boxService.findOne(15896);
+		Box handyWorkerBox = boxService.findOne(15912);
+		Box administratorBox = boxService.findOne(15905);
+
+		Assert.isTrue(customerBox.getMessages().size() == 1);
+		Assert.isTrue(handyWorkerBox.getMessages().size() == 1);
+		Assert.isTrue(administratorBox.getMessages().size() == 2);
 	}
 
-	// Administrator 1; Box 21 (Spam Box) -> 14753
-	// Administrator 1; Box 22 (In Box) -> 14754
-	// Administrator 1; Box 23 (Out Box) -> 14755
-	// Administrator 1; Box 24 (Trash Box) -> 14756
+	// Administrator 1; Box 21 (Spam Box) -> 15903
+	// Administrator 1; Box 22 (In Box) -> 15904
+	// Administrator 1; Box 23 (Out Box) -> 15905
+	// Administrator 1; Box 24 (Trash Box) -> 15906
 
-	// Customer 5; Box 17 (Spam Box) -> 14749
-	// Customer 5; Box 18 (In Box) -> 14750
-	// Customer 5; Box 19 (Out Box) -> 14751
-	// Customer 5; Box 20 (Trash Box) -> 14752
+	// Customer 5; Box 17 (Spam Box) -> 15899
+	// Customer 5; Box 18 (In Box) -> 15900
+	// Customer 5; Box 19 (Out Box) -> 15901
+	// Customer 5; Box 20 (Trash Box) -> 15902
 
 	@Test
 	public void testMessageToBox() {
 
-		Actor sender = actorService.findOne(14575); // Customer5
-		Actor recipient = actorService.findOne(14570); // Admin1
+		// authenticate("admin1");
+		// authenticate("customer5");
 
-		Box senderBox = boxService.findOne(14751);
-		Box recipientBox = boxService.findOne(14754);
+		Actor sender = actorService.findOne(15725); // Customer5
+		Actor recipient = actorService.findOne(15720); // Admin1
+
+		Box senderBox = boxService.findOne(15901);
+		Box recipientBox = boxService.findOne(15904);
 
 		Message message = messageService.create(sender, recipient);
 
@@ -109,5 +182,8 @@ public class MessageServiceTest extends AbstractTest {
 
 		Assert.isTrue(senderBox.getMessages().contains(message));
 		Assert.isTrue(recipientBox.getMessages().contains(message));
+
+		// unauthenticate();
+		// unauthenticate();
 	}
 }
