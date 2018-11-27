981edd2dd2a20f7adad1ca57d7b1e195c0b45b51
gonzaloguiher <zalo.agui1@gmail.com>
2018-11-27 11:14:14 +0100

    Servicio y Test de Box

--- Acme Handy Worker/src/main/java/services/BoxService.java
+++ Acme Handy Worker/src/main/java/services/BoxService.java
@@ -16,92 +16,83 @@ import domain.Actor;
 import domain.Box;
 import domain.Message;
 
-
 @Service
 @Transactional
 public class BoxService {
 
-	//Managed Repository -----
 	@Autowired
 	private BoxRepository boxRepository;
-	
-	//Supporting Services -----
-	
-	@Autowired
-	private ActorService actorService; 
-	
-	//Constructors -----
-	public BoxService(){
-		super();
+
+	public Box create(Actor actor) {
+
+		UserAccount userAccount = LoginService.getPrincipal();
+
+		Assert.isTrue(actor.getUserAccount().equals(userAccount));
+
+		Box box = new Box();
+
+		box.setActor(actor);
+		box.setSystemBox(false);
+		box.setMessages(new ArrayList<Message>());
+
+		return box;
 	}
-	
-	//Simple CRUD methods -----
-	public Box create(Actor a){
-		//Metodo general para todas los servicios, es probable 
-		//que sea necesario añadir atributos consistentes con la entity.
-		Box res = new Box();
-		res.setName("");
-		res.setSystemBox(false);
-		res.setActor(a);
-		res.setMessages(new ArrayList<Message>());
-		return res;
-	}
-	
-	public Collection<Box> findAll(){
+
+	public Collection<Box> findAll() {
 		return boxRepository.findAll();
 	}
-	
-	public Box findOne(int Id){
+
+	public Box findOne(int Id) {
 		return boxRepository.findOne(Id);
 	}
-	
-	public Box save(Box b){
-		
-		//Assert.isTrue(b.getSystemBox().equals(false));
-		
-		
+
+	public Box save(Box box) {
+
+		Box result;
+
 		UserAccount userAccount = LoginService.getPrincipal();
-		// modificar para aplicarlo a la entidad correspondiente.
-		//Assert.isTrue(b.getActor().getUserAccount().equals(userAccount));
-		
-		boxRepository.save(b);
-		return b;
+		Assert.isTrue(box.getActor().getUserAccount().equals(userAccount));
+
+		result = boxRepository.save(box);
+		return result;
 	}
-	
-	public void delete(Box b){
-		//puede necesitarse comprobar que el usuario que va a guardar el objeto es el dueño
-		//comprobar que el box no sea system box antes de borrarlo.
-		Assert.isTrue(!b.getSystemBox());
-		
+
+	public void delete(Box box) {
+
+		// Assert.isTrue(!b.getSystemBox());
+		Assert.isTrue(box.getSystemBox().equals(false));
+
 		UserAccount userAccount = LoginService.getPrincipal();
-		// modificar para aplicarlo a la entidad correspondiente.
-		// comprobar el dueño.
-		Assert.isTrue(b.getActor().getUserAccount().equals(userAccount));
-		
-		boxRepository.delete(b);
+		Assert.isTrue(box.getActor().getUserAccount().equals(userAccount));
+
+		boxRepository.delete(box);
 	}
-	
-	//Other business methods -----
-	
-	public Collection<Box> findByActorId (Integer actorId){
+
+	// Other business methods -----
+
+	public Collection<Box> findByActorId(Integer actorId) {
 		return boxRepository.findByActorId(actorId);
 	}
-	
-	public Box createUserBox(Actor a, String name){
-		Box res = this.create(a);
-		res.setName(name);
-		this.save(res);
+
+	public Box createUserBox(Actor actor) {
 		
-		return res;
+		Box box = this.create(actor);
+		
+		box.setName(box.getActor().getName());
+		this.save(box);
+
+		return box;
 	}
-	public void addMessageToBox(Box b, Message s){
-		List<Message> aux = new ArrayList<>(b.getMessages());
-		aux.add(0,s); // los mensajes nuevos siempre se ponen primero.
-		b.setMessages(aux);
-		
-		this.save(b);
-		
+
+	public void addMessageToBox(Box box, Message message) {
+
+		List<Message> aux = new ArrayList<>(box.getMessages());
+
+		aux.add(0, message); // los mensajes nuevos siempre se ponen primero.
+		box.setMessages(aux);
+
+		this.save(box);
+
 	}
-	
-	
+
 }
