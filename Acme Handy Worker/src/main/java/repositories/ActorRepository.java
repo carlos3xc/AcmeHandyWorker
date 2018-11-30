445a6e3bd4ddb1876d8fdf9a8881492fe9efc2de
Urabity <juaparser@gmail.com>
2018-11-30 12:21:30 +0100

    Quitados warnings amarillos repositorios, John

--- Acme Handy Worker/src/main/java/repositories/ActorRepository.java
+++ Acme Handy Worker/src/main/java/repositories/ActorRepository.java
@@ -1,7 +1,5 @@
 package repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import security.UserAccount;
 
 import domain.Actor;
-import domain.Finder;
 
 @Repository
 public interface ActorRepository extends JpaRepository<Actor, Integer>{
--- Acme Handy Worker/src/main/java/repositories/CustomerRepository.java
+++ Acme Handy Worker/src/main/java/repositories/CustomerRepository.java
@@ -1,14 +1,12 @@
 package repositories;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Customer;
-import domain.Referee;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
